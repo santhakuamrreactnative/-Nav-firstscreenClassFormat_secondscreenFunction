4f595c115313f084f49be7722cbba1264607667d
santhakumarreactnative <santhakumarreactnative@gmail.com>
2021-06-14 19:34:23 +0530

    navigation firstscreenClass and SecondScreen function

---
+++ App.js
@@ -0,0 +1,45 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component  {
+
+  constructor(props) {
+    super(props);
+}
+
+  render() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}  options={{title: 'Home Screen',  headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },}}/>
+        <Stack.Screen name="Details" component={DetailsScreen} options={{title: 'Details Screen', headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
+        }
+};
+
+
